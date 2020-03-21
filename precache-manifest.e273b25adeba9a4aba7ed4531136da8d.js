e64def418ecdba68b1c5dc6f9d389184b8607da0
Juan Pérez <jperezlondono@gmail.com>
2020-03-21 12:03:36 -0500

    Updates

---
+++ precache-manifest.e273b25adeba9a4aba7ed4531136da8d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1263fd72855ceab0a0be04ae1a4dd5e6",
+    "url": "/mrjusepelo.github.io/index.html"
+  },
+  {
+    "revision": "bc55cad8bba54cf3a7e7",
+    "url": "/mrjusepelo.github.io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "be82de98dc946498b650",
+    "url": "/mrjusepelo.github.io/static/js/2.b971f21b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mrjusepelo.github.io/static/js/2.b971f21b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc55cad8bba54cf3a7e7",
+    "url": "/mrjusepelo.github.io/static/js/main.4cb3a65a.chunk.js"
+  },
+  {
+    "revision": "de60ef2ed825cd4d77fa",
+    "url": "/mrjusepelo.github.io/static/js/runtime-main.81c84a7b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mrjusepelo.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.62d903c5ac4bd574d1acb70c246905b3.js"
+  "/mrjusepelo.github.io/precache-manifest.e273b25adeba9a4aba7ed4531136da8d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mrjusepelo.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
