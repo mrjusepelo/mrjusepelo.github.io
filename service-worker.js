8bec183c2b8b100b9c556676e1a56a9dea31eeb2
Juan Pérez <jperezlondono@gmail.com>
2020-03-20 21:12:47 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mrjusepelo/mrjusepelo.github.io/mrjusepelo/precache-manifest.f3dca78a0d4ac626305719a189e0f8f7.js"
+  "/mrjusepelo/mrjusepelo.github.io/precache-manifest.c6835b6cc796c7e4ae83b005257286b9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mrjusepelo/mrjusepelo.github.io/mrjusepelo/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mrjusepelo/mrjusepelo.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
