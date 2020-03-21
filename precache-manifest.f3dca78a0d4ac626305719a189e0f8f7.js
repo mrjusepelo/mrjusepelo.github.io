22b736eb5f944b91f86dc9afea4465feb1856254
Juan Pérez <jperezlondono@gmail.com>
2020-03-20 20:58:37 -0500

    Updates

---
+++ precache-manifest.f3dca78a0d4ac626305719a189e0f8f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3be788986709e8489e8517bd32dd8bd",
+    "url": "/mrjusepelo/mrjusepelo.github.io/mrjusepelo/index.html"
+  },
+  {
+    "revision": "c3c27709080868490dda",
+    "url": "/mrjusepelo/mrjusepelo.github.io/mrjusepelo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "be82de98dc946498b650",
+    "url": "/mrjusepelo/mrjusepelo.github.io/mrjusepelo/static/js/2.b971f21b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mrjusepelo/mrjusepelo.github.io/mrjusepelo/static/js/2.b971f21b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3c27709080868490dda",
+    "url": "/mrjusepelo/mrjusepelo.github.io/mrjusepelo/static/js/main.5c1d2634.chunk.js"
+  },
+  {
+    "revision": "3b3114d10f4244bf634d",
+    "url": "/mrjusepelo/mrjusepelo.github.io/mrjusepelo/static/js/runtime-main.f97a1159.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mrjusepelo/mrjusepelo.github.io/mrjusepelo/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mrjusepelo/precache-manifest.352928c7e55a8eabaaa14c32dde56731.js"
+  "/mrjusepelo/mrjusepelo.github.io/mrjusepelo/precache-manifest.f3dca78a0d4ac626305719a189e0f8f7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mrjusepelo/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mrjusepelo/mrjusepelo.github.io/mrjusepelo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
